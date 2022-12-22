5b4d180382a12eff8c793d045443891935f231e8
AndyP <malicioussoap@gmail.com>
2022-12-22 00:26:40 -0600

    workin but jankful

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from helpers import helpers
 import math, pygame, random, os
 
 
-with open("ascii_art/active_factory.txt") as active_factory_file:
-    active_factory_image = "".join(active_factory_file.readlines())
-with open("ascii_art/active_factory1.txt") as active_factory1_file:
-    active_factory1_image = "".join(active_factory1_file.readlines())
-with open("ascii_art/inactive_factory.txt") as inactive_factory_image:
+with open("ascii_art/A1.txt") as active_factory_file:
+    active_factory_image_1 = "".join(active_factory_file.readlines())
+with open("ascii_art/A2.txt") as active_factory1_file:
+    active_factory_image_2 = "".join(active_factory1_file.readlines())
+with open("ascii_art/A3.txt") as active_factory1_file:
+    active_factory_image_3 = "".join(active_factory1_file.readlines())
+with open("ascii_art/I.txt") as inactive_factory_image:
     inactive_factory_image = "".join(inactive_factory_image.readlines())
 
 times_manufactured = 0
@@ -24,10 +26,12 @@ while True:
     print(sawmill)
     if sawmill.manufacture():
         times_manufactured += 1
-        if times_manufactured % 2:
-            print(active_factory1_image)
-        else:
-            print(active_factory_image)
+        if times_manufactured % 3 ==0:
+            print(active_factory_image_1)
+        elif times_manufactured %3 == 1:
+            print(active_factory_image_2)
+        elif times_manufactured %3 == 2:
+            print(active_factory_image_3)
         print(f"successful manufacture, raw stock is now {helpers.truncate(sawmill.raw_stock)}, and manufactured stock is at {helpers.truncate(sawmill.manufactured_stock)}")
     else:
         print(inactive_factory_image)
