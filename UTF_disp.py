a6573ce725eea9ace515e4f5c06b777866054ea5
AndyP <malicioussoap@gmail.com>
2022-12-21 19:50:28 -0600

    drawn

--- UTF_disp.py
+++ UTF_disp.py
@@ -1,8 +1,7 @@
 with open("UTF_ALL.txt", "w") as f:
-    for i in range (69420):
-        print(chr(i), end = " , ")
-        f.write(f"{i},")
-    ꤲꤲꤲ
-#    ꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼꁼ
-#                                                                     ꁼꁼ
-                                                                       
+    try:
+        for i in range (69420):
+            print(chr(i), end = " , ")
+            f.write(f"{chr(i)}   ")
+    except Exception as e:
+        pass
